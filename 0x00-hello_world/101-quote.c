d01bc117db36af0869c960cfaf2edb1d7df7d16a
Bopha3949 <phila402@gmail.com>
2023-06-19 14:21:26 -0700

    kk

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -yati sengdziniwe mine bethuna
+ * return o (success)
+ */
+
+int main(void)
+{
+	printf('"and that piece of art is useful" - Dora Korpar, 2015-10-19"');
+	return (1)
+}
