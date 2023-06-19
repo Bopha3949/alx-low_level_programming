c920f72fb95c137814c66d92854257d154b5703f
Bopha3949 <phila402@gmail.com>
2023-06-19 09:36:13 -0700

    kk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - progam that print with proper grammar
- * Return o (success)
- */
+* main - progam that print with proper grammar
+* Return o (success)
+*/
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0)
