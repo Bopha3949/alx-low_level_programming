1b3d14711b487927acb003c105b2a8fd2b93ff75
bopha3949 <phila402@gmail.com>
2023-06-21 16:21:35 -0700

    lol

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,15 @@
 
 void print_alphabet_x10 (void);
 {
-
 	int i;
 	int c;
 
-	for (i = 1; i < = 10++);
+	for(i = 1; i < = 10++);
 	{
-		for (c = 1; c < = 10++);
+		for(c = 1; c < = 10++);
+		{
+			_putchar(C)
+		}
 	}
 	_putchar ('\n');
 }
