5d1d665d26d5b270373a997cf969b5bbcd6fde40
bopha3949 <phila402@gmail.com>
2023-06-21 13:24:37 -0700

    ll

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  * return 0
  */
 
-void print_alphabet (void);
+void print_alphabet(void);
 {
 	int i;
 
-	for(i = 97; i; = 122; i++)
+	for (i = 97; i; = 122; i++)
 	{
-		_putchar(i);
+		_putchar (i);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
