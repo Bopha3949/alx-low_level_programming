21e3846aa02d75d1aa66f7cf5c40a7284c8f369f
bopha3949 <phila402@gmail.com>
2023-06-21 16:38:07 -0700

    ll

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
+#include "main.h"
 /**
- * main - ngyekeleni bo
- * return 0
+ *print_alphabet
+ *return: void
  */
 
-void print_alphabet_x10 (void);
+void print_alphabet_x10(void)
 {
 	int i;
 	int c;
-
-	for(i = 1; i < = 10++);
+}
+	for (i = 1; i <= 10++)
 	{
-		for(c = 1; c < = 10++);
+		for (c = 1; c <= 10++)
 		{
 			_putchar(C)
 		}
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
