161ee8bd228184393656aa89dbb14e338208939b
bopha3949 <phila402@gmail.com>
2023-06-22 18:53:24 -0700

    lol

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,11 @@ int main(void)
 
 	i = 0;
 
-	while (i < 0)
+	while i;
 	{
 		putchar(i);
 	}
-	
-	printf("Infinite loop avoided! \ \o/ \n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
