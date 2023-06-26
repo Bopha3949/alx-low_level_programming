33a3d770666f92a8a10b2288dc0e958e63c5bcfc
bopha3949 <phila402@gmail.com>
2023-06-26 15:50:22 -0700

    lol

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - a function that takes a pointer
+ *
+ *return - 0
+ */
+void reset_to_98(int *n);
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset to 98(&n)
+	printf("n=%d\n", n);
+	return (0);
+}
