b0b1e2c384aedff64da23580686829ac597ed66c
bopha3949 <phila402@gmail.com>
2023-06-27 14:38:53 -0700

    lol

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -11,7 +11,7 @@ void swap_int(int *a, int *b)
 	*a = 98;
 	*b = 42;
 	
-	a = 1024; b = -4096;
-	a = INT_MAX; b = INT_MIN;
-	temp = *a;
+	*a = 1024; *b = -4096;
+	*a = INT_MAX; *b = INT_MIN;
+	temp = *a
 }
