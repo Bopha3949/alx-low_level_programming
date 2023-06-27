11b2398307371fbb3c89b20a7d1e18e3521f7417
bopha3949 <phila402@gmail.com>
2023-06-26 17:29:55 -0700

    lol

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-
-void updateValueTo98(int *ptr) {
-	*ptr = 98;
-	putchar('U');
-	putchar('p');
-	putchar('d');
-	putchar('a');
-	putchar('t');
-	putchar('e');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('o');
-	putchar(' ');
-	putchar('9');
-	putchar('8');
-	putchar('!');
-	putchar('\n');
+/**
+ * reset_to_98 - takes a pointer to an int and updates
+ *  the value it points to to 98
+ *
+ *  @n: an  inter input pointer
+ *  return: 0
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
 }
